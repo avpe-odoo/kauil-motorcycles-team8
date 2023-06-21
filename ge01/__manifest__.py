28e3d2340422df57a6e67b2725c47cb23512dcda
Albert Qi <alqi@odoo.com>
2023-06-21 12:10:42 -0400

    GE01

---
+++ ge01/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "GE01",
+    "summary": "Group Exercise 01",
+    "description": "Group exercise 01.",
+    "license": "LGPL-3",
+    "category": "Kawiil",
+    "depends": [
+        "base",
+        "motorcycle_registry",
+    ],
+    "data": [],
+    "demo": [],
+    "application": True,
+}
---
+++ ge01/models/stock_lot.py
@@ -0,0 +1,14 @@
+from odoo import api, fields, models
+
+
+class StockLot(models.Model):
+    _inherit = "stock.lot"
+
+    @api.model
+    def _get_next_serial(self, company, product):
+        product_type = product.product_tmpl_id.detailed_type
+        if product.tracking != "none" and product_type == "motorcycle":
+            prefix = product.make + product.model + str(product.year)[-2:]
+            return prefix + self.env["ir.sequence"].next_by_code("stock.lot.serial")
+        else:
+            return super(StockLot, self)._get_next_serial(company, product)
