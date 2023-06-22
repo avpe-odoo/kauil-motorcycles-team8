472466b1c0d8f4b3bd46dd43916db124b6697553
Albert Qi <alqi@odoo.com>
2023-06-22 14:23:10 -0400

    Update button validate

--- ge02/models/stock_picking.py
+++ ge02/models/stock_picking.py
@@ -6,15 +6,16 @@ class Picking(models.Model):
 
     def button_validate(self):
         if self.env["sale.order"].search([("name", "=", self.origin)]).state == "sale":
-            move_line_id = (
+            lot_record = (
                 self.env["stock.move.line"]
                 .search([("picking_id", "=", self.id)])
                 .lot_id.name
             )
             self.env["motorcycle.registry"].create(
                 {
-                    "vin": move_line_id,
+                    "vin": lot_record.name,
                     "owner_id": self.partner_id.id,
+                    "lot_id": lot_record,
                 }
             )
         return super(Picking, self).button_validate()
