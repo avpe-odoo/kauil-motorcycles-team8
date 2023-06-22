a5f6950b155d9d4d2b0a97a7b1d5cc29b5352638
Aviral Petwal <avpe@pop-os.localdomain>
2023-06-22 13:16:50 -0400

    GE 02 V2

--- ge01/models/stock_lot.py
+++ ge01/models/stock_lot.py
@@ -8,7 +8,7 @@ class StockLot(models.Model):
     def _get_next_serial(self, company, product):
         product_type = product.product_tmpl_id.detailed_type
         if product.tracking != "none" and product_type == "motorcycle":
-            prefix = product.make + product.model + str(product.year)[-2:]
+            prefix = product.make + product.model + str(product.year)[-2:]+str(product.battery_capacity).upper()
             return prefix + self.env["ir.sequence"].next_by_code("stock.lot.serial")
         else:
             return super(StockLot, self)._get_next_serial(company, product)
--- ge02/models/stock_picking.py
+++ ge02/models/stock_picking.py
@@ -3,19 +3,23 @@ from odoo import api, fields, models
 
 class Picking(models.Model):
     _inherit = "stock.picking"
-
+    
     def button_validate(self):
+        print()
+        print(self.move_line_ids)
+        print(self.env["stock.move.line"])
         if self.env["sale.order"].search([("name", "=", self.origin)]).state == "sale":
             print()
-            print(self.env["stock.lot"]
-                    .search([("name", "=", self.name)])
-                    .product_id)
-            print(self.name)
-            print()
+            print(self.move_line_ids)
+            temp_moveline_id=self.env["stock.move.line"].search([("picking_id", "=", self.id)]).lot_id.name
+            temp_partner_id=self.env["res.partner"]
+            print("LOLWA")
+            print(temp_moveline_id)
+            print("CHUl")
+            print(self.partner_id.name)
             self.env["motorcycle.registry"].create(
                 {
-                    "vin": self.env["stock.lot"]
-                    .search([("name", "=", self.name)])
-                    .product_id,
+                    "vin": temp_moveline_id,
+                    "owner_id":self.partner_id.id
                 }
             )
