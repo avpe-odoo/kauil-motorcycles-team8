dc67332a20dc3b525fb90108ddedffb241c0ae06
Aviral Petwal <avpe@pop-os.localdomain>
2023-06-23 14:28:45 -0400

    GE11

---
+++ ge11/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "GE11",
+    "summary": "Group Exercise 11",
+    "description": "Group exercise 11.",
+    "license": "LGPL-3",
+    "category": "Kawiil",
+    "depends": [
+        "base",
+        "motorcycle_registry",
+        "sale"
+    ],
+    "data": [
+        "views/sale_order_view.xml"
+    ],
+    "demo": [],
+    "application": True,
+}
